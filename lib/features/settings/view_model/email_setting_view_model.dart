9cb03436c2372612ca893fa965cd7bf7d95929cf
kimjunha1231 <tto9011@naver.com>
2025-03-08 21:04:44 +0900

    feat: 발송주기 페이지 제작

--- lib/features/settings/view_model/email_setting_view_model.dart
+++ lib/features/settings/view_model/email_setting_view_model.dart
@@ -7,6 +7,33 @@ final emailSettingProvider =
       (ref) => EmailSettingNotifier(),
     );
 
+// 이메일 발송 주기를 관리하는 Provider
+final emailFrequencyProvider =
+    StateNotifierProvider<EmailFrequencyNotifier, int>(
+      (ref) => EmailFrequencyNotifier(),
+    );
+
+class EmailFrequencyNotifier extends StateNotifier<int> {
+  static const String _frequencyKey = 'email_frequency';
+
+  EmailFrequencyNotifier() : super(7) {
+    // 기본값 7일
+    _loadFrequency();
+  }
+
+  Future<void> _loadFrequency() async {
+    final prefs = await SharedPreferences.getInstance();
+    final frequency = prefs.getInt(_frequencyKey) ?? 7;
+    state = frequency;
+  }
+
+  Future<void> saveFrequency(int days) async {
+    state = days;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_frequencyKey, days);
+  }
+}
+
 class EmailSettingNotifier extends StateNotifier<List<String>> {
   static const String _emailsKey = 'guardian_emails';
 
